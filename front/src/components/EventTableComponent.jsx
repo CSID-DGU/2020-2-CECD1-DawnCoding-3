2f590f44d2181dac88476de9a68f15dca06eab0c
jrucl2d <ysk789465@gmail.com>
2020-12-06 19:36:34 +0900

    아날로그 이벤트 덩어리 만들기

--- front/src/components/EventTableComponent.jsx
+++ front/src/components/EventTableComponent.jsx
@@ -9,7 +9,7 @@ import "./EventTableStyle.css";
 import { newEvents as newEventAction } from "../modules/newEvents";
 import { updateDevices, updateAnalog } from "../modules/devices";
 
-const StatusDeviceNum = 7; // 상태 정보를 갖는 디바이스의 개수(임의 설정)
+const StatusDeviceNum = 7; // 상태 정보를 갖는 디바이스의 개수
 
 function EventTableComponent() {
   const dispatch = useDispatch();
@@ -18,10 +18,25 @@ function EventTableComponent() {
   const theSetTimeout = useRef(null);
   const highLimitList = useRef(0);
   const lowLimitList = useRef(0);
+  const analogDeviceList = useRef([]);
 
   const [events, setEvents] = useState([]);
   const [watchMode, setWatchMode] = useState(false);
   const [ttsEnd, setTtsEnd] = useState(false);
+
+  // 아날로그 디바이스의 인덱스를 배열에 저장
+  useEffect(() => {
+    const deviceNumber = devices.length;
+    if (deviceNumber === 0) {
+      return;
+    }
+    Array(deviceNumber - StatusDeviceNum)
+      .fill()
+      // eslint-disable-next-line
+      .map((v, i) => {
+        analogDeviceList.current.push(i + StatusDeviceNum);
+      });
+  }, [devices]);
 
   // 새로운 이벤트 들어오면 화면에 표시
   useEffect(() => {
@@ -66,104 +81,105 @@ function EventTableComponent() {
   const makeSetTimeOut = (theTimeSeed) => {
     setTimeout(() => {
       // const isAnalog = Math.random() >= 0.5 ? true : false;
-      const isAnalog = false;
+      const isAnalog = true;
       if (isAnalog) {
         // 아날로그 데이터 -> 상한치/하한치를 랜덤하게 선택해서 해당 센서가 상한치/하한치 배열 안에 있다면 복귀 이벤트 발생, 아니면 초과 이벤트 발생
         // status: ["상한치 초과", "상한치 복귀", "하한치 초과", "하한치 복귀"];
-        const isUpper = Math.random() >= 0.5 ? true : false;
-        let theId = 0;
-        let theLimit = 0;
-        let eventStatus = "";
-        // if (isUpper) {
-        //   // 상한치 초과
-        //   if (highLimitList.current.length !== "") {
-        //     theId = highLimitList.current;
-        //     highLimitList.current = "";
-        //     eventStatus = `상한치 복귀`;
-        //     console.log(devices[theId]);
-        //     theLimit = devices[theId].highCriticalPoint - 5;
-        //   } else {
-        //     theId = Math.floor(Math.random() * 40) + 8;
-        //     highLimitList.current = theId;
-        //     eventStatus = `상한치 초과`;
-        //     theLimit = devices[theId].highCriticalPoint + 4;
-        //   }
-        // } else {
-        //   // 하한치 초과
-        //   if (lowLimitList.current !== "") {
-        //     theId = lowLimitList.current;
-        //     lowLimitList.current = "";
-        //     theLimit = devices[theId].lowCriticalPoint + 5;
-        //     console.log(devices[theId]);
-        //     eventStatus = `하한치 복귀`;
-        //   } else {
-        //     theId = Math.floor(Math.random() * 40) + 8;
-        //     theLimit = devices[theId].lowCriticalPoint - 4;
-        //     console.log(devices[theId]);
-        //     eventStatus = `하한치 초과`;
-        //     lowLimitList.current = theId;
-        //   }
-        // }
-
-        // 여기 임시로 그냥 강제
-        theId = 9;
-        if (highLimitList.current === 0) {
-          highLimitList.current = theId;
-          theLimit = devices[theId].highCriticalPoint + 4;
-          eventStatus = "상한치 초과";
-        } else {
-          theLimit = devices[theId].highCriticalPoint - 5;
-          highLimitList.current = 0;
-          eventStatus = "상한치 복귀";
-        }
-
+        const moreEventsNumber = 2 + Math.floor(Math.random() * 3);
         const theDate = new Date();
         const sendData = [];
-        sendData.push({
-          createDate: `${theDate.getFullYear()}-${theDate.getMonth()}-${theDate.getDate()} ${theDate.getHours()}:${theDate.getMinutes()}:${theDate.getSeconds()}.${theDate.getMilliseconds()}`,
-          deviceId: theId,
-          currValue: theLimit,
-          tts: true,
-        });
+        Array(moreEventsNumber)
+          .fill()
+          .forEach((v, i) => {
+            const isUpper = Math.random() >= 0.5 ? true : false;
+            let theId = 0;
+            let theLimit = 0;
+            let eventStatus = "";
+            if (isUpper) {
+              // 상한치 초과
+              if (highLimitList.current !== 0) {
+                theId = highLimitList.current;
+                analogDeviceList.current.push(theId);
+                highLimitList.current = 0;
+                eventStatus = `상한치 복귀`;
+                theLimit = devices[theId].highCriticalPoint - 5;
+              } else {
+                const theIndex = Math.floor(
+                  Math.random() * analogDeviceList.current.length
+                );
+                theId = analogDeviceList.current[theIndex];
+                analogDeviceList.current.splice(theIndex, 1);
+                highLimitList.current = theId;
+                eventStatus = `상한치 초과`;
+                theLimit = devices[theId].highCriticalPoint + 4;
+              }
+            } else {
+              // 하한치 초과
+              if (lowLimitList.current !== 0) {
+                theId = lowLimitList.current;
+                analogDeviceList.current.push(theId);
+                lowLimitList.current = 0;
+                theLimit = devices[theId].lowCriticalPoint + 5;
+                eventStatus = `하한치 복귀`;
+              } else {
+                const theIndex = Math.floor(
+                  Math.random() * analogDeviceList.current.length
+                );
+                theLimit = devices[theId].lowCriticalPoint - 4;
+                theId = analogDeviceList.current[theIndex];
+                analogDeviceList.current.splice(theIndex, 1);
+                eventStatus = `하한치 초과`;
+                lowLimitList.current = theId;
+              }
+            }
+            sendData.push({
+              createDate: `${theDate.getFullYear()}-${theDate.getMonth()}-${theDate.getDate()} ${theDate.getHours()}:${theDate.getMinutes()}:${theDate.getSeconds()}.${
+                theDate.getMilliseconds() + Math.floor(Math.random() * 10)
+              }`,
+              deviceId: theId,
+              currValue: theLimit,
+              tts: true,
+            });
+            // axios
+            //   .put("/device", sendData)
+            //   .then((res) => {
+            //     const theResult = res.data[0];
 
-        axios
-          .put("/device", sendData)
-          .then((res) => {
-            const theResult = res.data[0];
+            //     const result = [
+            //       {
+            //         createDate: sendData[0].createDate,
+            //         id: theResult.deviceId,
+            //         name: theResult.deviceName,
+            //         signalName: theResult.signalName,
+            //         status: eventStatus,
+            //         TTS: `${theResult.tts}`,
+            //         blink: true,
+            //         checked: false,
+            //       },
+            //     ];
 
-            const result = [
-              {
-                createDate: sendData[0].createDate,
-                id: theResult.deviceId,
-                name: theResult.deviceName,
-                signalName: theResult.signalName,
-                status: eventStatus,
-                TTS: `${theResult.tts}`,
-                blink: true,
-                checked: false,
-              },
-            ];
-
-            dispatch(newEventAction(result));
-            dispatch(
-              updateAnalog({
-                deviceId: theResult.deviceId,
-                currValue: theResult.currValue,
-              })
-            );
-            // TTS 요청
-            axios
-              .put("/device/tts", sendData)
-              .then((res) => {
-                // console.log(res);
-                setTtsEnd(!ttsEnd);
-              })
-              .catch((err) => console.log(err));
-          })
-          .catch((err) => console.error(err));
+            //     dispatch(newEventAction(result));
+            //     dispatch(
+            //       updateAnalog({
+            //         deviceId: theResult.deviceId,
+            //         currValue: theResult.currValue,
+            //       })
+            //     );
+            //     // TTS 요청
+            //     axios
+            //       .put("/device/tts", sendData)
+            //       .then((res) => {
+            //         // console.log(res);
+            //         setTtsEnd(!ttsEnd);
+            //       })
+            //       .catch((err) => console.log(err));
+            //   })
+            //   .catch((err) => console.error(err));
+          });
+        console.log(sendData);
       } else {
         // 상태 데이터의 감시 이벤트 발생
-        const theSelectedIndex = Math.floor(Math.random() * 7);
+        const theSelectedIndex = Math.floor(Math.random() * StatusDeviceNum);
         const theDate = new Date();
         const sendData = [];
         const settingData = [];
