c0b34cabce5bc53d3278fef42a60b61a14ade40f
jrucl2d <ysk789465@gmail.com>
2020-12-08 21:10:54 +0900

    상태 순위 변경 로직 추가

--- front/src/components/ModalWindow/StatusModalComponent.jsx
+++ front/src/components/ModalWindow/StatusModalComponent.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Button, Form, Modal } from "react-bootstrap";
+import { v4 as uuid } from 'uuid';
 
 function StatusModalComponent({
   show,
@@ -9,15 +10,26 @@ function StatusModalComponent({
   onClickClose,
 }) {
   const [changeMode, setChangeMode] = useState(true);
+  const [orderValue, setOrderValue] = useState([])
+
+  useEffect(() => {
+    // 현재 단순히 index로 우선순위 처리 -> 나중에 변경 필요
+    const newOrderValue = Object.entries(selectedDevice.statuses).map((v, i) => i)
+    setOrderValue(newOrderValue);
+  }, [selectedDevice])
 
   const onClickChangeMode = () => {
     setChangeMode(!changeMode);
   };
 
-  const onClickOrderChange = () => {};
+  const onClickOrderChange = () => { };
+
+  const onChangeOrderValue = (e) => {
+    setOrderValue(orderValue.map((v, i) => i === +e.target.name ? e.target.value : v))
+  }
 
   return changeMode ? (
-    <Modal show={show} onHide={() => {}}>
+    <Modal show={show} onHide={() => { }}>
       <Modal.Header>
         <Modal.Title>{selectedDevice.deviceName}</Modal.Title>
       </Modal.Header>
@@ -58,53 +70,56 @@ function StatusModalComponent({
       </Modal.Footer>
     </Modal>
   ) : (
-    <Modal show={show} onHide={() => {}}>
-      <Modal.Header>
-        <Modal.Title>{selectedDevice.deviceName}</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <table>
-          <thead style={{ borderTop: "1px solid black" }}>
-            <tr>
-              <th>상태 명</th>
-              <th>우선순위</th>
-            </tr>
-          </thead>
-          <tbody>
-            {Object.entries(selectedDevice.statuses).map((v, i) => (
+      <Modal show={show} onHide={() => { }}>
+        <Modal.Header>
+          <Modal.Title>{selectedDevice.deviceName}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <table>
+            <thead style={{ borderTop: "1px solid black" }}>
               <tr>
-                <td>{v[1]}</td>
-                <td>
-                  <input
-                    style={{ width: "100%", border: "none" }}
-                    type="text"
-                    value={i}
-                  />
-                </td>
+                <th>상태 명</th>
+                <th>우선순위</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button variant="warning" onClick={onClickChangeMode}>
-          상태 제어창
+            </thead>
+            <tbody>
+              {Object.entries(selectedDevice.statuses).map((v, i) => (
+                <tr key={uuid()}
+                >
+                  <td>{v[1]}</td>
+                  <td>
+                    <input
+                      style={{ width: "100%", border: "none" }}
+                      type="text"
+                      name={i}
+                      value={orderValue[i]}
+                      onChange={onChangeOrderValue}
+                    />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="warning" onClick={onClickChangeMode}>
+            상태 제어창
         </Button>
-        <Button variant="primary" onClick={onClickOrderChange}>
-          우선순위 변경
+          <Button variant="primary" onClick={onClickOrderChange}>
+            우선순위 변경
         </Button>
-        <Button
-          variant="secondary"
-          onClick={() => {
-            onClickClose();
-            setChangeMode(true);
-          }}
-        >
-          닫기
+          <Button
+            variant="secondary"
+            onClick={() => {
+              onClickClose();
+              setChangeMode(true);
+            }}
+          >
+            닫기
         </Button>
-      </Modal.Footer>
-    </Modal>
-  );
+        </Modal.Footer>
+      </Modal>
+    );
 }
 
 export default StatusModalComponent;
