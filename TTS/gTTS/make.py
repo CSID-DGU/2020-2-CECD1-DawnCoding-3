c50cbcaa9d1b5732c40d5988a4ae33ed1fe9842a
Bae Jin Woo <jw969696@naver.com>
2021-02-20 02:35:26 +0900

    타코트론 삭제

--- TTS/gTTS/make.py
+++
@@ -1,8 +0,0 @@
-from gtts import gTTS
-
-
-def makeSound(textList, fileName):
-    text = textList
-    tts = gTTS(text=text, lang="ko")
-    # filename = f"{fileName}.mp3"
-    tts.save(f"{fileName}.mp3")
--- TTS/gTTS/sapi.py
+++
@@ -1,9 +0,0 @@
-import win32com.client
-import sys
-# import time
-
-# start = time.time()
-message = sys.argv[1]
-tts = win32com.client.Dispatch("SAPI.SpVoice")
-tts.Speak(message)
-# print("time :", time.time() - start)
