d62dbbdf9ca63d695698e036099862cea39c3f33
jrucl2d <ysk789465@gmail.com>
2021-02-21 14:18:36 +0900

    STT 결과 화면 수정

--- server2/stt/app.py
+++ server2/stt/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request
 from sqlalchemy import create_engine, text
-from command import querySelector, start
+# from command import querySelector, start
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
 
@@ -11,9 +11,15 @@ app.database = database
 @app.route('/stt')
 def hello_word():
     command, query = start()
+    # command, query = ('어떤 커맨드라고 알려줘', 'aa')
 
     print(query)
     rows = app.database.execute(text(query)).fetchall()
+    # rows = app.database.execute(text(
+    #     '''
+    #     select * from device where analog=0x01
+    #     '''
+    # )).fetchall();
     res = [{
         'deviceId': row['device_id'],
         'analog': row['analog'],
